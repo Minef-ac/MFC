fffcc3f8436da5422d1c2599b149a7d8400751f4
phaseman <gamerlukenrip@gmail.com>
2021-05-05 19:27:09 -0400

    Added sparky listener and depdendency back in! I want to use it :)
    Also I removed luckperms from the depends list as I just check for a permission instead of using any api.

---
+++ src/main/java/ac/minef/mfc/spigot/listeners/SparkyPunish.java
@@ -0,0 +1,22 @@
+package ac.minef.mfc.spigot.listeners;
+
+import ac.minef.mfc.spigot.MFC;
+import ac.sparky.api.events.SparkyPunishEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class SparkyPunish implements Listener {
+
+    @EventHandler
+    public void onSparkyPunish(SparkyPunishEvent e) {
+        e.setCancelled(true);
+        MFC.getInstance().getServer().broadcastMessage("\n&6✘ &cMinef.ac &ehas removed&c "
+                + e.getPlayer().getName() + "&e for cheating &6✘\n");
+        if (!e.getPlayer().hasPermission("group.administrator")) {
+            MFC.getInstance().executeCommand("ipban -s " + e.getPlayer().getName()
+                    + " 30d Blacklisted Modifications\n[" + e.getCheckName() + ","
+                    + e.getCheckType() + "," + e.getCategory() + "," + e.getViolation() + "VL]");
+        }
+    }
+
+}
